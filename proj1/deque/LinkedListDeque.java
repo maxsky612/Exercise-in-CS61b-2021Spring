3bb1369e4a076363676de5248e3b1998df7760b8
maxsky <maxsky612@gmail.com>
2022-09-20 15:47:10 +0800

    Create LinkedListDeque
    Create Linked Lists with Circular Sentinel

---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,89 @@
+package deque;
+
+public class LinkedListDeque<type> {
+    public class Node {
+        type item;
+        Node prev;
+        Node next;
+
+        public Node(type x, Node y, Node z){
+            item = x;
+            prev = y;
+            next = z;
+        }
+    }
+    private int size = 0;
+    private Node sentinel;
+
+    public LinkedListDeque(type a){
+        sentinel = new Node(null, null, null);
+        sentinel.next = new Node(a, sentinel, sentinel);
+        sentinel.prev = sentinel.next;
+        size += 1;
+    }
+
+    public LinkedListDeque(){
+        sentinel = new Node(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+    }
+
+    public void addFirst(type a){
+        size += 1;
+        sentinel.next = new Node(a, sentinel, sentinel.next);
+        sentinel.next.next.prev = sentinel.next;
+    }
+
+    public void addLast(type a){
+        size += 1;
+        sentinel.prev = new Node(a, sentinel.prev, sentinel);
+        sentinel.prev.prev.next = sentinel.prev;
+    }
+
+    public type getFirst(){
+        return sentinel.next.item;
+    }
+
+    public type getLast(){
+        return sentinel.prev.item;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public type removeFirst(){
+        size -= 1;
+        type a = sentinel.next.item;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        return a;
+    }
+
+    public type removeLast(){
+        size -= 1;
+        type a = sentinel.prev.item;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        return a;
+    }
+
+    public static void main(String[] args){
+        LinkedListDeque a = new LinkedListDeque();
+        LinkedListDeque b = new LinkedListDeque(1);
+        for (int i = 0; i < 5; i += 1){
+            a.addFirst(i + 1);
+        }
+        for (int i = 1; i < 5; i += 1){
+            b.addFirst(i + 1);
+        }
+        for (int i = 0; i < 3; i += 1){
+            a.addLast(i + 1);
+            b.addLast(i + 1);
+        }
+        a.removeFirst();
+        a.removeLast();
+        b.removeFirst();
+        b.removeLast();
+    }
+}
