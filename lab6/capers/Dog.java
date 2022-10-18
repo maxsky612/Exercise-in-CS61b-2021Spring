e3df1a03c939d04f916eaf609e5336f9ca48938b
maxsky <maxsky612@gmail.com>
2022-10-18 22:21:37 +0800

    pass self-test but fail make-check

---
+++ lab6/capers/Dog.java
@@ -0,0 +1,69 @@
+package capers;
+
+import java.io.File;
+import java.io.Serializable;
+import static capers.Utils.*;
+
+/** Represents a dog that can be serialized.
+ * @author Hao Wang
+*/
+public class Dog implements Serializable {
+
+    /** Folder that dogs live in. */
+    static final File DOG_FOLDER = join(CapersRepository.CAPERS_FOLDER, "dogs");
+
+    /** Age of dog. */
+    private int age;
+    /** Breed of dog. */
+    private String breed;
+    /** Name of dog. */
+    private String name;
+
+    /**
+     * Creates a dog object with the specified parameters.
+     * @param name Name of dog
+     * @param breed Breed of dog
+     * @param age Age of dog
+     */
+    public Dog(String name, String breed, int age) {
+        this.age = age;
+        this.breed = breed;
+        this.name = name;
+    }
+
+    /**
+     * Reads in and deserializes a dog from a file with name NAME in DOG_FOLDER.
+     *
+     * @param name Name of dog to load
+     * @return Dog read from file
+     */
+    public static Dog fromFile(String name) {
+        // TODO (hint: look at the Utils file)
+        File f = join(DOG_FOLDER, name);
+        return readObject(f, Dog.class);
+    }
+
+    /**
+     * Increases a dog's age and celebrates!
+     */
+    public void haveBirthday() {
+        age ++;
+        System.out.println(toString());
+        System.out.println("Happy birthday! Woof! Woof!");
+    }
+
+    /**
+     * Saves a dog to a file for future use.
+     */
+    public void saveDog() {
+        writeObject(join(DOG_FOLDER, this.name), this);
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+            "Woof! My name is %s and I am a %s! I am %d years old! Woof!",
+            name, breed, age);
+    }
+
+}
